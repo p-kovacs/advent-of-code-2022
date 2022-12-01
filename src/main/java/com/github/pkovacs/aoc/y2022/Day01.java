78e004e5ebfb9603ee3284228181f20d59fe34db
Péter Kovács <kovacspeter84@gmail.com>
2022-12-01 09:00:51 +0100

    Add solution for Day 1

--- src/main/java/com/github/pkovacs/aoc/y2022/Day01.java
+++ src/main/java/com/github/pkovacs/aoc/y2022/Day01.java
@@ -1,15 +1,25 @@
 package com.github.pkovacs.aoc.y2022;
 
+import java.util.List;
+
 import com.github.pkovacs.aoc.AocUtils;
 import com.github.pkovacs.util.InputUtils;
 
 public class Day01 {
 
     public static void main(String[] args) {
-        var lines = InputUtils.readLines(AocUtils.getInputPath());
+        var blocks = InputUtils.collectLineBlocks(InputUtils.readString(AocUtils.getInputPath()));
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        System.out.println("Part 1: " + solve(blocks, 1));
+        System.out.println("Part 2: " + solve(blocks, 3));
+    }
+
+    private static long solve(List<List<String>> blocks, int count) {
+        return blocks.stream()
+                .mapToLong(block -> block.stream().mapToLong(Long::parseLong).sum())
+                .sorted()
+                .skip(blocks.size() - count)
+                .sum();
     }
 
 }
