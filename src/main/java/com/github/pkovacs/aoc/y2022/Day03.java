ceefafda282fdfed6ee2ed7bc8a3574ec1a78316
Péter Kovács <kovacspeter84@gmail.com>
2022-12-03 06:20:47 +0100

    Add solution for Day 3

--- src/main/java/com/github/pkovacs/aoc/y2022/Day03.java
+++ src/main/java/com/github/pkovacs/aoc/y2022/Day03.java
@@ -1,15 +1,40 @@
 package com.github.pkovacs.aoc.y2022;
 
+import java.util.HashSet;
+import java.util.List;
+
 import com.github.pkovacs.aoc.AocUtils;
 import com.github.pkovacs.util.InputUtils;
+import com.google.common.collect.Lists;
 
 public class Day03 {
 
     public static void main(String[] args) {
         var lines = InputUtils.readLines(AocUtils.getInputPath());
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        System.out.println("Part 1: " + solve(lines.stream().map(Day03::split).toList()));
+        System.out.println("Part 2: " + solve(Lists.partition(lines, 3)));
+    }
+
+    private static List<String> split(String str) {
+        int n = str.length() / 2;
+        return List.of(str.substring(0, n), str.substring(n));
+    }
+
+    private static int solve(List<List<String>> lists) {
+        return lists.stream().map(Day03::getCommonChar).mapToInt(Day03::getPriority).sum();
+    }
+
+    private static char getCommonChar(List<String> lists) {
+        var set = new HashSet<>(Lists.charactersOf(lists.get(0)));
+        for (int i = 1; i < lists.size(); i++) {
+            set.retainAll(new HashSet<>(Lists.charactersOf(lists.get(i))));
+        }
+        return set.iterator().next(); // assume that we always have a single common character
+    }
+
+    private static int getPriority(char ch) {
+        return ch >= 'a' && ch <= 'z' ? ch - 'a' + 1 : ch - 'A' + 27;
     }
 
 }
--- src/test/java/com/github/pkovacs/aoc/y2022/AocSolutionsTest.java
+++ src/test/java/com/github/pkovacs/aoc/y2022/AocSolutionsTest.java
@@ -21,7 +21,7 @@ public class AocSolutionsTest {
         return Stream.of(
                 new Arguments("Day01", Day01::main, "72718", "213089"),
                 new Arguments("Day02", Day02::main, "10624", "14060"),
-                new Arguments("Day03", Day03::main, "0", "0"),
+                new Arguments("Day03", Day03::main, "8139", "2668"),
                 new Arguments("Day04", Day04::main, "0", "0"),
                 new Arguments("Day05", Day05::main, "0", "0"),
                 new Arguments("Day06", Day06::main, "0", "0"),
