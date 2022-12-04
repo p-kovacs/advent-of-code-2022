f1f06114c58d5a1147d86330111eee650fcbefcf
Péter Kovács <kovacspeter84@gmail.com>
2022-12-04 13:50:36 +0100

    Add solution for Day 4

--- src/main/java/com/github/pkovacs/aoc/y2022/Day04.java
+++ src/main/java/com/github/pkovacs/aoc/y2022/Day04.java
@@ -8,8 +8,23 @@ public class Day04 {
     public static void main(String[] args) {
         var lines = InputUtils.readLines(AocUtils.getInputPath());
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        int cnt1 = 0, cnt2 = 0;
+        for (var line : lines) {
+            var parts = line.split("[,-]");
+            var a = Integer.parseInt(parts[0]);
+            var b = Integer.parseInt(parts[1]);
+            var c = Integer.parseInt(parts[2]);
+            var d = Integer.parseInt(parts[3]);
+            if ((c >= a && d <= b) || (c <= a && d >= b)) {
+                cnt1++;
+            }
+            if (Math.max(a, c) <= Math.min(b, d)) {
+                cnt2++;
+            }
+        }
+
+        System.out.println("Part 1: " + cnt1);
+        System.out.println("Part 2: " + cnt2);
     }
 
 }
