46c3ef4cd2e25d65c9b47b78ce50ef4463139310
Péter Kovács <kovacspeter84@gmail.com>
2022-12-21 15:41:31 +0100

    Simplify solution for Day 19

--- src/main/java/com/github/pkovacs/aoc/y2022/Day19.java
+++ src/main/java/com/github/pkovacs/aoc/y2022/Day19.java
@@ -1,9 +1,6 @@
 package com.github.pkovacs.aoc.y2022;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.IntStream;
 
 import com.github.pkovacs.aoc.AocUtils;
@@ -67,17 +64,17 @@ public class Day19 {
 
         int calculate(int maxTime) {
             bestResult = 0;
-            dfs(new State(new byte[] { 0, 0, 0, 0 }, new byte[] { 1, 0, 0, 0 }, maxTime));
+            dfs(new byte[] { 0, 0, 0, 0 }, new byte[] { 1, 0, 0, 0 }, maxTime);
             return bestResult;
         }
 
-        void dfs(State s) {
-            var list = new ArrayList<State>();
-
-            int idleResult = s.res[GEODE] + s.robots[GEODE] * s.time;
+        void dfs(byte[] res, byte[] robots, int time) {
+            // Update best result
+            int idleResult = res[GEODE] + robots[GEODE] * time;
             bestResult = Math.max(bestResult, idleResult);
 
-            int maxResult = idleResult + s.time * (s.time - 1) / 2;
+            // Check upper bound for this branch
+            int maxResult = idleResult + time * (time - 1) / 2;
             if (maxResult <= bestResult) {
                 return;
             }
@@ -85,68 +82,34 @@ public class Day19 {
             for (int i = 3; i >= 0; i--) {
                 // Attempt to build a robot of type i as the next one (after waiting 0 or more minutes as necessary)
                 var cost = costs[i];
-                if (s.robots[i] < maxRobotCount[i]
-                        && s.time - 1 >= minRobotRemainingTime[i]
-                        && IntStream.range(0, 4).allMatch(j -> cost[j] == 0 || s.robots[j] > 0)) {
+                if (robots[i] == maxRobotCount[i]
+                        || IntStream.range(0, 4).anyMatch(j -> cost[j] > 0 && robots[j] == 0)) {
+                    continue;
+                }
 
-                    int timeToWait = Math.max(0,
-                            IntStream.range(0, 4)
-                                    .filter(j -> cost[j] > 0)
-                                    .map(j -> (cost[j] - s.res[j] + s.robots[j] - 1) / s.robots[j])
-                                    .max().orElseThrow());
+                int timeToWait = Math.max(0,
+                        IntStream.range(0, 4)
+                                .filter(j -> cost[j] > 0)
+                                .map(j -> (cost[j] - res[j] + robots[j] - 1) / robots[j])
+                                .max().orElseThrow());
 
-                    int time = s.time - 1 - timeToWait;
-                    if (time >= minRobotRemainingTime[i]) {
-                        var res = s.res.clone();
-                        IntStream.range(0, 4).forEach(j -> res[j] += (timeToWait + 1) * s.robots[j] - cost[j]);
-                        var robots = s.robots.clone();
-                        robots[i]++;
-                        list.add(new State(res, robots, time));
-                    }
+                int newTime = time - 1 - timeToWait;
+                if (newTime >= minRobotRemainingTime[i]) {
+                    var newRes = res.clone();
+                    IntStream.range(0, 4).forEach(j -> newRes[j] += (timeToWait + 1) * robots[j] - cost[j]);
+                    var newRobots = robots.clone();
+                    newRobots[i]++;
+
+                    dfs(newRes, newRobots, newTime);
 
                     // Additional heuristic: if we can build a geode robot without waiting, then we should do that,
                     // other options can be ignored. And it's also true for the first instance of any other robot type.
-                    boolean importantRobot = i == GEODE || s.robots[i] == 0;
-                    if (!list.isEmpty() && timeToWait == 0 && importantRobot) {
+                    boolean importantRobot = i == GEODE || robots[i] == 0;
+                    if (timeToWait == 0 && importantRobot) {
                         break;
                     }
                 }
             }
-
-            list.forEach(this::dfs);
-        }
-
-    }
-
-    private static final class State {
-
-        final byte[] res;
-        final byte[] robots;
-        final int time;
-
-        final int hash;
-
-        State(byte[] res, byte[] robots, int time) {
-            this.res = res;
-            this.robots = robots;
-            this.time = time;
-            hash = Objects.hash(Arrays.hashCode(res), Arrays.hashCode(robots), time);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null || obj.getClass() != this.getClass()) {
-                return false;
-            }
-            var other = (State) obj;
-            return this.time == other.time
-                    && Arrays.equals(res, other.res)
-                    && Arrays.equals(robots, other.robots);
-        }
-
-        @Override
-        public int hashCode() {
-            return hash;
         }
 
     }
