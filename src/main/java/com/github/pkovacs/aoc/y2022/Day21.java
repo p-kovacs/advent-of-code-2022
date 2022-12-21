00ba8a39f7a99f6e1d29602012fc80ea2c7a04c0
Péter Kovács <kovacspeter84@gmail.com>
2022-12-21 07:19:45 +0100

    Add solution for Day 21

--- src/main/java/com/github/pkovacs/aoc/y2022/Day21.java
+++ src/main/java/com/github/pkovacs/aoc/y2022/Day21.java
@@ -1,5 +1,8 @@
 package com.github.pkovacs.aoc.y2022;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.github.pkovacs.aoc.AocUtils;
 import com.github.pkovacs.util.InputUtils;
 
@@ -8,8 +11,57 @@ public class Day21 {
     public static void main(String[] args) {
         var lines = InputUtils.readLines(AocUtils.getInputPath());
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        var monkeys = lines.stream().collect(Collectors.toMap(s -> s.substring(0, 4), s -> s.substring(6)));
+
+        System.out.println("Part 1: " + eval1(monkeys, "root"));
+        System.out.println("Part 2: " + eval2(monkeys, "root", 0));
+    }
+
+    private static long eval1(Map<String, String> monkeys, String m) {
+        if (!monkeys.get(m).contains(" ")) {
+            return Long.parseLong(monkeys.get(m));
+        } else {
+            var p = monkeys.get(m).split(" ");
+            return switch (p[1].charAt(0)) {
+                case '+' -> eval1(monkeys, p[0]) + eval1(monkeys, p[2]);
+                case '-' -> eval1(monkeys, p[0]) - eval1(monkeys, p[2]);
+                case '*' -> eval1(monkeys, p[0]) * eval1(monkeys, p[2]);
+                case '/' -> eval1(monkeys, p[0]) / eval1(monkeys, p[2]);
+                default -> throw new IllegalArgumentException();
+            };
+        }
+    }
+
+    private static long eval2(Map<String, String> monkeys, String m, long goal) {
+        if (!monkeys.get(m).contains(" ")) {
+            return goal; // it must be "humn"
+        } else {
+            var p = monkeys.get(m).split(" ");
+            var humLeft = hasHuman(monkeys, p[0]);
+            var hum = humLeft ? p[0] : p[2];
+            var refValue = eval1(monkeys, humLeft ? p[2] : p[0]);
+
+            if (m.equals("root")) {
+                return eval2(monkeys, hum, refValue);
+            } else {
+                return switch (p[1].charAt(0)) {
+                    case '+' -> eval2(monkeys, hum, goal - refValue);
+                    case '-' -> eval2(monkeys, hum, humLeft ? goal + refValue : refValue - goal);
+                    case '*' -> eval2(monkeys, hum, goal / refValue);
+                    case '/' -> eval2(monkeys, hum, humLeft ? goal * refValue : refValue / goal);
+                    default -> throw new IllegalArgumentException();
+                };
+            }
+        }
+    }
+
+    private static boolean hasHuman(Map<String, String> monkeys, String m) {
+        if (!monkeys.get(m).contains(" ")) {
+            return m.equals("humn");
+        } else {
+            var p = monkeys.get(m).split(" ");
+            return hasHuman(monkeys, p[0]) || hasHuman(monkeys, p[2]);
+        }
     }
 
 }
